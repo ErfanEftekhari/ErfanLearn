bd60ded32493c16004d8308e257738ca6d32b07f
Erfan <erfan.eftekharizade@gmail.com>
2022-04-05 23:20:58 +0430

    Start Project

---
+++ ErfanLearn.DataLayer/Context/ErfanLearnContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ErfanLearn.DataLayer.Entities.User;
+
+namespace ErfanLearn.DataLayer.Context
+{
+   public class ErfanLearnContext:DbContext
+    {
+
+        public ErfanLearnContext(DbContextOptions<ErfanLearnContext> options):base(options)
+        {
+            
+        }
+
+        #region User
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+
+        #endregion
+
+    }
+}
---
+++ ErfanLearn/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ErfanLearn.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello");
+        }
+    }
+}
